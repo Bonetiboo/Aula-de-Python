57b6c4ca1a1051da8fd9014dd5f4d93d61d95678
Rafael <rafaceriano123@gmail.com>
2026-03-17 17:15:02 -0300

    corrigindo pasta de exercicios

---
+++ exercicios-de-python/Ex 1.py
@@ -0,0 +1,11 @@
+# Ex.1 - Calcule a área de um cone.
+
+pi = 3.14
+r = float(input("Forneça o raio:"))
+h = float(input("Forneça a altura:"))
+a = pi * r ** 2 * h
+
+print ("A área é igual à:", a)
+
+
+
