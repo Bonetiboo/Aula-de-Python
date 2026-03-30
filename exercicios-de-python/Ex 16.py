5e3fdb1ac7b7d11121bd3c3ba3a88ac66431369d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 19:27:24 -0300

    melhoria no exercicio 16

--- exercicios-de-python/Ex 16.py
+++ exercicios-de-python/Ex 16.py
@@ -1,9 +1,9 @@
 # Ex.16 - Solicite valor e se a pessoa é vip ou não. Se valor maior ou igual a 200, crie estrutura de condição aninhada para verificar se a pessoa é vip, se for, ofereça 20% de desconto sobre o valor e mostre o valor a ser descontado e o valor final considerando o desconto. Se não for vip, ofereça o desconto de 10%.
 
 valor = float(input("Digite o valor do produto:"))
-vip = input("Você é VIP? (responda com sim ou não):")
 
 if valor >= 200:
+    (vip := input("Você é VIP? (responda com sim ou não):"))
     if vip == "sim":
         print(f"Você recebeu um desconto de 20%, ou seja {valor/5}, no total ficaria {valor - (valor/5)}")
     else:
